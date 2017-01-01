42536c8d84fdd67c4a71990e98c177162177bd4d
AndriusK <akrisiun@gmail.com>
2017-01-01 15:16:41 +0200

    /bin/sh sample

--- docker-wine/run-ssh.sh
+++ docker-wine/run-ssh.sh
@@ -1,6 +1,9 @@
 
+#!/bin/sh
+
 #    && echo -e "AllowUsers git\n" >> /etc/ssh/sshd_config \
 #    && echo -e "Port 22\n" >> /etc/ssh/sshd_config
+#    mono /usr/bin/*.exe "$@"
 
 mkdir /var/run/sshd
 /usr/sbin/sshd -D -e -f /etc/ssh/sshd_config
